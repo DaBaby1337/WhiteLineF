94df9dd5612a5ed8642aa1d2af32d5ac406b23f8
Nikita Denisov <rok-08@bk.ru>
2022-03-10 21:59:43 +0400

    slider-mouse-done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const images= document.querySelectorAll('.slider .slider-line img');
 const sliderLine= document.querySelector('.slider-line');
 let count=0;
+let timeoutId;
 let width;
 function init(){
     console.log('resize');
@@ -15,23 +16,58 @@ function init(){
 window.addEventListener('resize',init);
 init();
 
-document.querySelector('.slider-next').addEventListener('click', function(){
+  document.querySelector('.slider-next').addEventListener('click', function(){
     count++;
     if (count>= images.length){
         count=0;
     }
-rollSlider();
+rollSlider(count);
 })
-
 document.querySelector('.slider-back').addEventListener('click', function(){
     count--;
     if (count <0 ){
         count=images.length-1;
     }
-rollSlider();
+rollSlider(count);
 })
-
-function rollSlider(){
-    sliderLine.style.transform ='translate(-' +count*width +'px';
+function rollSlider(count){
+        sliderLine.style.transform ='translate(-' +count*width +'px';
+    }
+ function start(){
+     count++;
+     if (count>= images.length){
+        count=0;
+    }
+    if (count <0 ){
+        count=images.length-1;
+    }
+    rollSlider(count);
 }
+    document.getElementById('sliderW').onmouseenter=function(){
+    start();
+}
+// document.getElementById('sliderW').onmouseleave=function(){
+//     start();
+// }
+
+// function rollSlider(count){
+//     sliderLine.style.transform ='translate(-' +count*width +'px';
+// }
 // SLIDER-END
+// setTimeout( rollSlider,2000,1);
+
+// function rollSlider(count,stop){
+//     if (stop=1){clearTimeout(timerId);}
+//     timerId = setTimeout(function request() {
+//     if (count>= images.length){
+//         count=0;
+//     }
+//     if (count <0 ){
+//         count=images.length-1;
+//     }  
+//     sliderLine.style.transform ='translate(-' +count*width +'px';
+//   count++;
+//   timerId = setTimeout(request, delay);
+//   if (stop=1){clearTimeout(timerId);}
+// }, delay);
+// }
